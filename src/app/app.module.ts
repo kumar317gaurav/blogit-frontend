c06b3421e32cec3a34c6c8fd89befbe9d7bcbfa6
kumar317gaurav <kumar317gaurav@gmail.com>
2024-10-08 23:25:15 +0530

    homepage ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { HeroComponent } from './features/hero/hero.component';
+import { MainComponent } from './features/main/main.component';
+import { SidebarComponent } from './features/sidebar/sidebar.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeroComponent,
+    MainComponent,
+    SidebarComponent,
+    
   ],
   imports: [
     BrowserModule,
