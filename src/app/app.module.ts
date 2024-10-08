044154dcffbfb84ad3f7452e60e9990caf56e946
gourav-96 <gouravkrsingh4096@gmail.com>
2024-10-08 16:51:08 +0530

    update login route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,18 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { SignupComponent } from './features/sign-up/sign-up.component';
 import { AppRoutingModule } from './app-routing.module';
+import { LoginComponent } from './features/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SignupComponent
+    SignupComponent,
+    LoginComponent 
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule 
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
