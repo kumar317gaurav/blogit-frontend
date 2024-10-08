53ba863cf577097462d7b28c0a5f6ef028a27771
gourav-96 <gouravkrsingh4096@gmail.com>
2024-10-08 22:26:47 +0530

    update login route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,15 @@ import { AppComponent } from './app.component';
 import { SignupComponent } from './features/sign-up/sign-up.component';
 import { AppRoutingModule } from './app-routing.module';
 import { NavbarComponent } from './shared/navbar/navbar.component';
-import { HeroComponent } from './features/hero/hero.component';
-import { MainComponent } from './features/main/main.component';
-import { SidebarComponent } from './features/sidebar/sidebar.component';
+import { LoginComponent } from './features/login/login.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     SignupComponent,
-    NavbarComponent,
-    HeroComponent,
-    MainComponent,
-    SidebarComponent
+    LoginComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
