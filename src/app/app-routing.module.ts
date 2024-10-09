59fa7d9e94fda2ce58d1dd9b453e75000f3b81f3
gourav-96 <gouravkrsingh4096@gmail.com>
2024-10-09 22:52:32 +0530

    create-blog component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SignupComponent } from './features/sign-up/sign-up.component';
 import { LoginComponent } from './features/login/login.component';
+import { CreateBlogComponent } from './features/create-blog/create-blog.component';
 
 const routes: Routes = [
   { path: 'signup', component: SignupComponent },
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'create-blog', component: CreateBlogComponent }
 ];
 
 @NgModule({
